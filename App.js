a369386e93b035186aaffb570244a734ee7f752b
Chudier <chudierp@gmail.com>
2022-03-18 09:59:38 -0700

    onKeyPressed, cells activated

--- App.js
+++ App.js
@@ -1,39 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-
+import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
+import { useState } from 'react';
 
 import {colors} from './src/constants';
 import  Keyboard from './src/components/Keyboard';
+import { borderColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 const number_of_tries = 4
+
+const copyArray = (arr) => {
+  return [...arr.map((rows) => [...rows])];
+}
 
 export default function App() {
   const word = 'hello';
   const letters = word.split(''); //returns an array of characters
 
-  const rows = new Array(number_of_tries).fill(
-    new Array(letters.length).fill('a')
-  );
+  const [rows, setRows] = useState(
+    new Array(number_of_tries).fill(new Array(letters.length).fill(''))
+    );
   
+  const [curRow, setCurRow] = useState(0);
+  const [curCol, setCurCol] = useState(0);
+  
+  const onKeyPressed = (key) => {
+    const updateRows = copyArray(rows);
+    updateRows[curRow][curCol] = key;
+    setRows(updateRows);
+    setCurCol(curCol + 1);
+  };
+
+  const isCellActive = (row, col) => {
+    return row === curRow && col === curCol;
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
       <Text style={styles.title}>w o r d l e</Text>
       
-      <View style={styles.map}>
-
-        {rows.map((row) => (
+      <ScrollView style={styles.map}>
+        {rows.map((row, i) => (
           <View style={styles.row}>
-            {row.map((cell) => ( //for each letter, render a cell 
-            <View style={styles.cell} >
+            {row.map((cell, j) => ( //for each letter, render a cell 
+              <View 
+                style={[
+                  styles.cell,
+                  {
+                    borderColor: isCellActive(i,j)
+                      ? colors.lightgrey
+                      : colors.darkgrey,
+                  },
+                ]} 
+              >
               <Text style={styles.cellText}>{cell.toUpperCase()}</Text>
             </View>
           ))} 
           </View>
         ))}
-      </View>
+      </ScrollView>
 
-      <Keyboard /> 
+      <Keyboard onKeyPressed={onKeyPressed} /> 
     </SafeAreaView>
   );
 }
@@ -51,7 +78,8 @@ const styles = StyleSheet.create({
   },
   map: {
     alignSelf: 'stretch',
-    height: 100
+    height: 100,
+    marginVertical: 20
   },
   row: {
     alignSelf: 'stretch',
